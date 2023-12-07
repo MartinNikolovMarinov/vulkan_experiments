75c1c5bcc1443caa39988406894ce7fdc8da3a4d
Martin Marinov <martin_n_marinov@protonmail.com>
2023-12-08 00:39:38 +0200

    Texture mapping 1

--- include/init_core.h
+++ include/init_core.h
@@ -18,10 +18,10 @@ using namespace coretypes;
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE // Vulkan uses depth range 0 to 1
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+// #define GLM_FORCE_RADIANS
+// #define GLM_FORCE_DEPTH_ZERO_TO_ONE // Vulkan uses depth range 0 to 1
+// #include <glm/vec4.hpp>
+// #include <glm/mat4x4.hpp>
 
 #include <vulkan/vulkan.h>
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <init_core.h>
 
 #include <cstdlib>
@@ -40,8 +43,13 @@ enum ErrorType : i32 {
     VulkanMapMemoryFailed,
     VulkanDescriptorPoolCreationFailed,
     VulkanDescriptorSetAllocationFailed,
+    VulkanTextureImageCreationFailed,
+    VulkanTextureImageMemoryAllocationFailed,
+    VulkanTextureImageMemoryBindingFailed,
 
     FailedToLoadShader,
+
+    STBIFailedToLoadImage,
 
     SENTINEL
 };
@@ -84,8 +92,13 @@ const char* errorTypeToCptr(ErrorType t) {
         case VulkanMapMemoryFailed:                    return "VulkanMapMemoryFailed";
         case VulkanDescriptorPoolCreationFailed:       return "VulkanDescriptorPoolCreationFailed";
         case VulkanDescriptorSetAllocationFailed:      return "VulkanDescriptorSetAllocationFailed";
+        case VulkanTextureImageCreationFailed:         return "VulkanTextureImageCreationFailed";
+        case VulkanTextureImageMemoryAllocationFailed: return "VulkanTextureImageMemoryAllocationFailed";
+        case VulkanTextureImageMemoryBindingFailed:    return "VulkanTextureImageMemoryBindingFailed";
 
         case FailedToLoadShader:                       return "FailedToLoadShader";
+
+        case STBIFailedToLoadImage:                    return "STBIFailedToLoadImage";
 
         case SENTINEL: return "SENTINEL";
     }
@@ -469,56 +482,6 @@ core::expected<Error> createBuffer(VkPhysicalDevice pdevice, VkDevice device, Vk
     return {};
 }
 
-core::expected<Error> copyBuffer(VkDevice device, VkCommandPool commandPool, VkQueue graphicsQueue,
-                                 VkBuffer src, VkBuffer dst, VkDeviceSize size) {
-    VkCommandBufferAllocateInfo allocInfo{};
-    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-    allocInfo.commandPool = commandPool;
-    allocInfo.commandBufferCount = 1;
-
-    VkCommandBuffer commandBuffer;
-    if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
-        return core::unexpected<Error>({ "Vulkan command buffer allocation failed", VulkanCommandBufferCreationFailed });
-    }
-
-    defer {
-        vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
-    };
-
-    VkCommandBufferBeginInfo beginInfo{};
-    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-
-    if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {
-        return core::unexpected<Error>({ "Vulkan command buffer begin failed", VulkanBeginCommandBufferFailed });
-    }
-
-        VkBufferCopy copyRegion{};
-        copyRegion.srcOffset = 0;
-        copyRegion.dstOffset = 0;
-        copyRegion.size = size;
-        vkCmdCopyBuffer(commandBuffer, src, dst, 1, &copyRegion);
-
-    if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
-        return core::unexpected<Error>({ "Vulkan command buffer end failed", VulkanEndCommandBufferFailed });
-    }
-
-    VkSubmitInfo submitInfo{};
-    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &commandBuffer;
-
-    if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE) != VK_SUCCESS) {
-        return core::unexpected<Error>({ "Vulkan queue submit failed", VulkanEndCommandBufferFailed });
-    }
-    if (vkQueueWaitIdle(graphicsQueue) != VK_SUCCESS) {
-        return core::unexpected<Error>({ "Vulkan queue wait idle failed", VulkanEndCommandBufferFailed });
-    }
-
-    return {};
-}
-
 struct Application {
 
 #ifndef NDEBUG
@@ -661,6 +624,10 @@ private:
         }
 
         if (auto res = createCommandPool(); res.hasErr()) {
+            return core::unexpected<Error>(core::move(res.err()));
+        }
+
+        if (auto res = createTextureImage(); res.hasErr()) {
             return core::unexpected<Error>(core::move(res.err()));
         }
 
@@ -1380,6 +1347,148 @@ private:
         return {};
     }
 
+    core::expected<Error> createTextureImage() {
+        constexpr const char* TEXTURE_PATH = ASSETS_PATH "/textures/texture.jpg";
+        i32 texW, texH, texChannels;
+
+        stbi_uc* pixels = stbi_load(TEXTURE_PATH, &texW, &texH, &texChannels, STBI_rgb_alpha);
+        if (!pixels) {
+            return core::unexpected<Error>({ "Failed to load texture image", STBIFailedToLoadImage });
+        }
+        defer { stbi_image_free(pixels); };
+
+        VkDeviceSize imageSize = texW * texH * 4;
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
+        {
+            VkMemoryPropertyFlags props = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                                          VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+            VkBufferUsageFlags usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+            auto res = createBuffer(m_vkPhysicalDevice, m_vkDevice, imageSize,
+                                    usage, props, stagingBuffer, stagingBufferMemory);
+            if (res.hasErr()) {
+                return core::unexpected<Error>(core::move(res.err()));
+            }
+        }
+
+        defer {
+            vkDestroyBuffer(m_vkDevice, stagingBuffer, nullptr);
+            vkFreeMemory(m_vkDevice, stagingBufferMemory, nullptr);
+        };
+
+        void* data;
+        if (vkMapMemory(m_vkDevice, stagingBufferMemory, 0, imageSize, 0, &data) != VK_SUCCESS) {
+            return core::unexpected<Error>({ "Vulkan texture image mapping failed", VulkanMapMemoryFailed });
+        }
+            core::memcopy(data, pixels, imageSize);
+        vkUnmapMemory(m_vkDevice, stagingBufferMemory);
+
+        {
+            auto res = createImage(texW, texH, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+                                   VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+                                   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_vkTextureImage, m_vkTextureImageMemory);
+            if (res.hasErr()) {
+                return core::unexpected<Error>(core::move(res.err()));
+            }
+        }
+
+        transitionImageLayout(m_vkTextureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED,
+                              VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+        copyBufferToImage(stagingBuffer, m_vkTextureImage, u32(texW), u32(texH));
+
+        transitionImageLayout(m_vkTextureImage, VK_FORMAT_R8G8B8A8_SRGB,
+                              VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                              VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+        return {};
+    }
+
+    core::expected<Error> createImage(u32 width, u32 height, VkFormat format, VkImageTiling tiling,
+                                      VkImageUsageFlags usage, VkMemoryPropertyFlags props, VkImage& image,
+                                      VkDeviceMemory& imageMemory) {
+        VkImageCreateInfo imageInfo{};
+        imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+        imageInfo.imageType = VK_IMAGE_TYPE_2D;
+        imageInfo.extent.width = width;
+        imageInfo.extent.height = height;
+        imageInfo.extent.depth = 1;
+        imageInfo.mipLevels = 1;
+        imageInfo.arrayLayers = 1;
+        imageInfo.format = format;
+        imageInfo.tiling = tiling;
+        imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        imageInfo.usage = usage;
+        imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+        imageInfo.flags = 0;
+
+        if (vkCreateImage(m_vkDevice, &imageInfo, nullptr, &image) != VK_SUCCESS) {
+            return core::unexpected<Error>({ "Vulkan texture image creation failed", VulkanTextureImageCreationFailed });
+        }
+
+        VkMemoryRequirements memRequirements;
+        vkGetImageMemoryRequirements(m_vkDevice, image, &memRequirements);
+
+        u32 memTypeIndex;
+        {
+            auto res = findMemoryType(m_vkPhysicalDevice, memRequirements.memoryTypeBits, props);
+            if (res.hasErr()) {
+                return core::unexpected<Error>(core::move(res.err()));
+            }
+            memTypeIndex = core::move(res.value());
+        }
+
+        VkMemoryAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.allocationSize = memRequirements.size;
+        allocInfo.memoryTypeIndex = memTypeIndex;
+
+        if (vkAllocateMemory(m_vkDevice, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS) {
+            return core::unexpected<Error>({ "Vulkan texture image memory allocation failed", VulkanTextureImageMemoryAllocationFailed });
+        }
+
+        if (vkBindImageMemory(m_vkDevice, m_vkTextureImage, imageMemory, 0) != VK_SUCCESS) {
+            return core::unexpected<Error>({ "Vulkan texture image memory binding failed", VulkanTextureImageMemoryBindingFailed });
+        }
+
+        return {};
+    }
+
+    VkCommandBuffer beginSingleTimeCommands() {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = m_vkCommandPool;
+        allocInfo.commandBufferCount = 1;
+
+        VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(m_vkDevice, &allocInfo, &commandBuffer);
+
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+        return commandBuffer;
+    }
+
+    void endSingleTimeCommands(VkCommandBuffer commandBuffer) {
+        vkEndCommandBuffer(commandBuffer);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        vkQueueSubmit(m_vkGraphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+        vkQueueWaitIdle(m_vkGraphicsQueue);
+
+        vkFreeCommandBuffers(m_vkDevice, m_vkCommandPool, 1, &commandBuffer);
+    }
+
     core::expected<Error> createVertexBuffer() {
         VkDeviceSize bufferSize = m_vertices.byteLen();
 
@@ -1420,8 +1529,7 @@ private:
         }
 
         {
-            auto res = copyBuffer(m_vkDevice, m_vkCommandPool, m_vkGraphicsQueue,
-                                  stagingBuffer, m_vkVertexBuffer, bufferSize);
+            auto res = copyBuffer(stagingBuffer, m_vkVertexBuffer, bufferSize);
             if (res.hasErr()) {
                 return core::unexpected<Error>(core::move(res.err()));
             }
@@ -1470,8 +1578,7 @@ private:
         }
 
         {
-            auto res = copyBuffer(m_vkDevice, m_vkCommandPool, m_vkGraphicsQueue,
-                                  stagingBuffer, m_vkIndexBuffer, bufferSize);
+            auto res = copyBuffer(stagingBuffer, m_vkIndexBuffer, bufferSize);
             if (res.hasErr()) {
                 return core::unexpected<Error>(core::move(res.err()));
             }
@@ -1638,6 +1745,90 @@ private:
         }
 
         return {};
+    }
+
+    core::expected<Error> copyBuffer(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
+        VkCommandBuffer commandBuffer = beginSingleTimeCommands();
+
+        VkBufferCopy copyRegion{};
+        copyRegion.srcOffset = 0;
+        copyRegion.dstOffset = 0;
+        copyRegion.size = size;
+        vkCmdCopyBuffer(commandBuffer, src, dst, 1, &copyRegion);
+
+        endSingleTimeCommands(commandBuffer);
+
+        return {};
+    }
+
+    void transitionImageLayout(VkImage image, VkFormat, VkImageLayout oldLayout, VkImageLayout newLayout) {
+        VkCommandBuffer commandBuffer = beginSingleTimeCommands();
+
+        VkImageMemoryBarrier barrier{};
+        barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+        barrier.oldLayout = oldLayout;
+        barrier.newLayout = newLayout;
+
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+
+        barrier.image = image;
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        barrier.subresourceRange.baseMipLevel = 0;
+        barrier.subresourceRange.levelCount = 1;
+        barrier.subresourceRange.baseArrayLayer = 0;
+        barrier.subresourceRange.layerCount = 1;
+
+        VkPipelineStageFlags sourceStage;
+        VkPipelineStageFlags destinationStage;
+
+        if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+            barrier.srcAccessMask = 0;
+            barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+            sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+            destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        }
+        else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+            barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+            barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+            sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+            destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+        }
+        else {
+            Assert(false, "unsupported layout transition!");
+        }
+
+        vkCmdPipelineBarrier(commandBuffer,
+                             sourceStage, destinationStage,
+                             0,
+                             0, nullptr,
+                             0, nullptr,
+                             1, &barrier);
+
+        endSingleTimeCommands(commandBuffer);
+    }
+
+    void copyBufferToImage(VkBuffer buffer, VkImage image, u32 width, u32 height) {
+        VkCommandBuffer commandBuffer = beginSingleTimeCommands();
+
+        VkBufferImageCopy region{};
+        region.bufferOffset = 0;
+        region.bufferRowLength = 0;
+        region.bufferImageHeight = 0;
+
+        region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.imageSubresource.mipLevel = 0;
+        region.imageSubresource.baseArrayLayer = 0;
+        region.imageSubresource.layerCount = 1;
+
+        region.imageOffset = { 0, 0, 0 };
+        region.imageExtent = { width, height, 1 };
+
+        vkCmdCopyBufferToImage(commandBuffer, buffer, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+
+        endSingleTimeCommands(commandBuffer);
     }
 
     void cleanupSwapChain() {
@@ -1844,6 +2035,9 @@ private:
 
         cleanupSwapChain();
 
+        vkDestroyImage(m_vkDevice, m_vkTextureImage, nullptr);
+        vkFreeMemory(m_vkDevice, m_vkTextureImageMemory, nullptr);
+
         for (addr_size i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
             vkDestroyBuffer(m_vkDevice, m_vkUniformBuffers[i], nullptr);
             vkFreeMemory(m_vkDevice, m_vkUniformBuffersMemory[i], nullptr);
@@ -1935,6 +2129,8 @@ private:
     core::Arr<void*> m_vkUniformBuffersMapped;
     VkDescriptorPool m_vkDescriptorPool = VK_NULL_HANDLE;
     core::Arr<VkDescriptorSet> m_vkDescriptorSets;
+    VkImage m_vkTextureImage;
+    VkDeviceMemory m_vkTextureImageMemory;
 };
 
 // NEXT: Start from here -> https://vulkan-tutorial.com/Texture_mapping/Images
