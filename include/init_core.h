1267db5c1f7cbc6208d86b4602534903eb3967e3
Martin Marinov <martin_n_marinov@protonmail.com>
2023-12-08 20:35:32 +0200

    added tinyobjloader and created wrapper for it. Created wrapper for stb too.

--- include/init_core.h
+++ include/init_core.h
@@ -14,14 +14,16 @@ using namespace coretypes;
 
 #include <fmt/core.h>
 #include <fmt/color.h>
-
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include <tiny_obj_loader.h>
+#include <stb_image.h>
 
 // #define GLM_FORCE_RADIANS
 // #define GLM_FORCE_DEPTH_ZERO_TO_ONE // Vulkan uses depth range 0 to 1
 // #include <glm/vec4.hpp>
 // #include <glm/mat4x4.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <vulkan/vulkan.h>
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
 #include <init_core.h>
 
 #include <cstdlib>
